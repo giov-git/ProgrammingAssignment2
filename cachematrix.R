4a8767f8ecba68e182692f1e3348f3c3689ed546
gio <ascarigiovanni@hotmail.com>
2020-05-06 16:07:49 +0200

    edit cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##The scripts includes 2 functions. 
+## makeCachematrix takes a matrix as argument. Then it initialise the variable inverse equal to null. 
+## the set function takes y as an argument and assign it the element x in the parent environment (the matrix). Inverse (in the parent environment) is confirmed as null
+## the get function retrieves the matrix defined in the parent environment
+## the function setinverse takes the argument solve and assign it to the object inverse in the parent environment
+## the function getinverse retrieves the value of inverse
+## the it creates a list when it assigns a name to the 4 previous functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() {x}
+  
+  setinverse <- function(solve) {
+    inverse <<- solve }
+  
+  getinverse <- function() {
+    inverse}
+  
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##cachemean must retrieve the function solve of the matrix
+## it puts inverse equal to the element defined in makeCacheMatrix's list
+## it controls if it's equal to null. If it's the case it calculates solve, otherwise it retrieves the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
+
+##Now i check id it's working
+matrix = makeCacheMatrix(matrix(c(15,20,25,30), nrow = 2, ncol = 2))
+matrix$get()
+matrix$set()
+cacheSolve(matrix)
+
+      
